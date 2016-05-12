20f4f8def229a2f0c773e7d89e38ef793bdb20c7
frazkaka <mickefransson1@gmail.com>
2016-05-12 14:36:46 +0200

    Gjort top-header från scratch. Vad tycks än så länge?

--- index.php
+++ index.php
@@ -3,25 +3,35 @@
 <head>
  <meta charset="utf-8">
          <title>Projektets hemsida</title>
-           <link rel="icon" href="bakgrund.jpg">
+         <link rel="icon" href="bakgrund.jpg">
          <link rel="stylesheet" type="text/css" href="stilmall.css">
+         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
 </head>
 <body>
-<header>
-
-    <div id="header">
-      <span>
-          <ul>
-          <li>START</li>
-          </li>RECEPT</li>
-        </ul>
-      </span>
-
+  <header>
+    <div class="nav">
+      <ul>
+        <li class="logo">PluggTugg</li>
+        <li class="home"><a href="#">Start</a></li>
+        <li class="contact"><a href="#">Kontakta oss</a></li>
+        <li class="menu1"><a href="#">menu1</a></li>
+        <li class="menu2"><a href="#">menu2</a></li>
+        <li class="menu3"><a href="#">menu3</a></li>
+      </ul>
+      <div class="searchField">
+        <form>
+          <div class="inner-addon left-addon">
+            <i class="glyphicon glyphicon-search"></i>
+            <input type="text" class="form-search" placeholder="Sök i databasen"/>
+        </form>
+      </div>
     </div>
->>>>>>> master
-<div id="footer">
-  contact us @ cheaphealthfood@proes.com
-</div>
+  </header>
+
 </body>
 
 
