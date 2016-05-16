0b3178875682dcf6df5816698773add9f212ebda
frazkaka <mickefransson1@gmail.com>
2016-05-16 11:31:52 +0200

    lite av varje

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>Ocean goes fruitbasket</title>
-
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="stilmall.css">
+
          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
          <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
@@ -56,9 +56,15 @@
       </nav>
     </header>
         <!-- ----------------------------------------------- -->
-    <div id='divContainer'>
-    TESTAR SKRIVS
-  </div>
+        <div id="container">
+
+          <div id='leftContainer'>
+            Top 10
+          </div>
+          <div id='rightContainer'>
+            Flöde av bilder
+          </div>
+        </div>
 
   </body>
 </html>
