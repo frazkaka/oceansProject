42b05b816cd330877a1162c24fe9ef0015f28623
frazkaka <mickefransson1@gmail.com>
2016-05-24 10:41:44 +0200

    ändrat till '-tecken

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 
-    <?php include "html-elements/html_head.php";
-include "html-elements/html_nav.php";?>
+    <?php include 'html-elements/html_head.php';
+include 'html-elements/html_nav.php';?>
 
     <div id='container'>
       <div id='leftContainer'>Top 10</div>
       <div id='rightContainer'>Flöde av bilder</div>
     </div>
- <?php include"html-elements/html_footer.php";?>
+ <?php include'html-elements/html_footer.php';?>
--- recept.php
+++ recept.php
@@ -1,74 +1,74 @@
-    <?php include "html-elements/html_head.php";
-include "html-elements/html_nav.php";?>
+    <?php include 'html-elements/html_head.php';
+include 'html-elements/html_nav.php';?>
     <!-- Page Content -->
-    <div class="container">
+    <div class='container'>
 
         <!-- Page Header -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Recept
+        <div class='row'>
+            <div class='col-lg-12'>
+                <h1 class='page-header'>Recept
                     <small>Alla</small>
                 </h1>
             </div>
         </div>
           <!-- navbar-collapse -->
-        <div class="col-lg-12 bordered" class="collapse navbar-collapse" id="categories">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Alla</a></li>
-            <li><a href="#">Kött</a></li>
-            <li><a href="#">Fågel</a></li>
-            <li><a href="#">Fisk/Skaldjur</a></li>
-            <li><a href="#">Pasta</a></li>
-            <li><a href="#">Soppa/pajer</a></li>
-            <li><a href="#">Vegetariskt</a></li>
-            <li><a href="#">Mackor/wraps</a></li>
-            <li><a href="#">Pannkakor/omelett</a></li>
-            <li><a href="#">Mellanmål</a></li>
-            <li><a href="#">Övrigt</a></li>
+        <div class='col-lg-12 bordered' class='collapse navbar-collapse' id='categories'>
+          <ul class='nav navbar-nav'>
+            <li class='active'><a href='#'>Alla</a></li>
+            <li><a href='#'>Kött</a></li>
+            <li><a href='#'>Fågel</a></li>
+            <li><a href='#'>Fisk/Skaldjur</a></li>
+            <li><a href='#'>Pasta</a></li>
+            <li><a href='#'>Soppa/pajer</a></li>
+            <li><a href='#'>Vegetariskt</a></li>
+            <li><a href='#'>Mackor/wraps</a></li>
+            <li><a href='#'>Pannkakor/omelett</a></li>
+            <li><a href='#'>Mellanmål</a></li>
+            <li><a href='#'>Övrigt</a></li>
           </ul>
         </div>
         <!-- /.navbar-collapse -->
         <!-- searchbox -->
-        <div class="row" id="searchfilter">
-          <div class="btn-group btn-group-sm col-lg-3" data-toggle="buttons" id="price">
+        <div class='row' id='searchfilter'>
+          <div class='btn-group btn-group-sm col-lg-3' data-toggle='buttons' id='price'>
             <h5>Pris (kr)</h5>
-            <label class="btn btn-default">
-              <input type="checkbox" checked> 1-5
+            <label class='btn btn-default'>
+              <input type='checkbox' checked> 1-5
             </label>
-            <label class="btn btn-default"> 6-10
-              <input type="checkbox">
+            <label class='btn btn-default'> 6-10
+              <input type='checkbox'>
             </label>
-            <label class="btn btn-default"> 11-15
-              <input type="checkbox">
+            <label class='btn btn-default'> 11-15
+              <input type='checkbox'>
             </label>
-            <label class="btn btn-default"> 16-20
-              <input type="checkbox">
+            <label class='btn btn-default'> 16-20
+              <input type='checkbox'>
             </label>
-            <label class="btn btn-default"> 21-30
-              <input type="checkbox">
+            <label class='btn btn-default'> 21-30
+              <input type='checkbox'>
             </label>
           </div>
-          <div class="btn-group btn-group-sm col-lg-3" data-toggle="buttons" id="price">
+          <div class='btn-group btn-group-sm col-lg-3' data-toggle='buttons' id='price'>
             <h5>Tid (min)</h5>
-            <label class="btn btn-default">
-              <input type="checkbox" checked> 0-10
+            <label class='btn btn-default'>
+              <input type='checkbox' checked> 0-10
             </label>
-            <label class="btn btn-default"> 11-20
-              <input type="checkbox">
+            <label class='btn btn-default'> 11-20
+              <input type='checkbox'>
             </label>
-            <label class="btn btn-default"> 20-40
-              <input type="checkbox">
+            <label class='btn btn-default'> 20-40
+              <input type='checkbox'>
             </label>
-            <label class="btn btn-default"> 40 +
-              <input type="checkbox">
+            <label class='btn btn-default'> 40 +
+              <input type='checkbox'>
             </label>
           </div>
-          <div class="col-lg-6" id="custom-search-input">
-                 <div class="input-group col-lg-14">
-                     <input type="text" class="form-control input-lg" placeholder="Sök recept" />
-                     <span class="input-group-btn">
-                         <button class="btn btn-warning btn-lg" type="button">
-                             <span class=" glyphicon glyphicon-search"></span>
+          <div class='col-lg-6' id='custom-search-input'>
+                 <div class='input-group col-lg-14'>
+                     <input type='text' class='form-control input-lg' placeholder='Sök recept' />
+                     <span class='input-group-btn'>
+                         <button class='btn btn-warning btn-lg' type='button'>
+                             <span class=' glyphicon glyphicon-search'></span>
                          </button>
                      </span>
                  </div>
@@ -76,31 +76,31 @@ include "html-elements/html_nav.php";?>
         </div>
         <!-- /.searchbox -->
         <!-- Projects Row -->
-        <div class="row">
-            <div class="col-md-4 portfolio-item">
-                <a href="#">
-                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+        <div class='row'>
+            <div class='col-md-4 portfolio-item'>
+                <a href='#'>
+                    <img class='img-responsive' src='http://placehold.it/700x400' alt=''>
                 </a>
                 <h3>
-                    <a href="#">Project Name</a>
+                    <a href='#'>Project Name</a>
                 </h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
             </div>
-            <div class="col-md-4 portfolio-item">
-                <a href="#">
-                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+            <div class='col-md-4 portfolio-item'>
+                <a href='#'>
+                    <img class='img-responsive' src='http://placehold.it/700x400' alt=''>
                 </a>
                 <h3>
-                    <a href="#">Project Name</a>
+                    <a href='#'>Project Name</a>
                 </h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
             </div>
-            <div class="col-md-4 portfolio-item">
-                <a href="#">
-                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+            <div class='col-md-4 portfolio-item'>
+                <a href='#'>
+                    <img class='img-responsive' src='http://placehold.it/700x400' alt=''>
                 </a>
                 <h3>
-                    <a href="#">Project Name</a>
+                    <a href='#'>Project Name</a>
                 </h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
             </div>
@@ -108,62 +108,62 @@ include "html-elements/html_nav.php";?>
         <!-- /.row -->
 
         <!-- Projects Row -->
-        <div class="row">
-            <div class="col-md-4 portfolio-item">
-                <a href="#">
-                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+        <div class='row'>
+            <div class='col-md-4 portfolio-item'>
+                <a href='#'>
+                    <img class='img-responsive' src='http://placehold.it/700x400' alt=''>
                 </a>
                 <h3>
-                    <a href="#">Project Name</a>
+                    <a href='#'>Project Name</a>
                 </h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
             </div>
-            <div class="col-md-4 portfolio-item">
-                <a href="#">
-                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+            <div class='col-md-4 portfolio-item'>
+                <a href='#'>
+                    <img class='img-responsive' src='http://placehold.it/700x400' alt=''>
                 </a>
                 <h3>
-                    <a href="#">Project Name</a>
+                    <a href='#'>Project Name</a>
                 </h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
             </div>
-            <div class="col-md-4 portfolio-item">
-                <a href="#">
-                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+            <div class='col-md-4 portfolio-item'>
+                <a href='#'>
+                    <img class='img-responsive' src='http://placehold.it/700x400' alt=''>
                 </a>
                 <h3>
-                    <a href="#">Project Name</a>
+                    <a href='#'>Project Name</a>
                 </h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
             </div>
         </div>
 
         <!-- Projects Row -->
-        <div class="row">
-            <div class="col-md-4 portfolio-item">
-                <a href="#">
-                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+        <div class='row'>
+            <div class='col-md-4 portfolio-item'>
+                <a href='#'>
+                    <img class='img-responsive' src='http://placehold.it/700x400' alt=''>
                 </a>
                 <h3>
-                    <a href="#">Project Name</a>
+                    <a href='#'>Project Name</a>
                 </h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
             </div>
-            <div class="col-md-4 portfolio-item">
-                <a href="#">
-                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+            <div class='col-md-4 portfolio-item'>
+                <a href='#'>
+                    <img class='img-responsive' src='http://placehold.it/700x400' alt=''>
                 </a>
                 <h3>
-                    <a href="#">Project Name</a>
+                    <a href='#'>Project Name</a>
                 </h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
             </div>
-            <div class="col-md-4 portfolio-item">
-                <a href="#">
-                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+            <div class='col-md-4 portfolio-item'>
+                <a href='#'>
+                    <img class='img-responsive' src='http://placehold.it/700x400' alt=''>
                 </a>
                 <h3>
-                    <a href="#">Project Name</a>
+                    <a href='#'>Project Name</a>
                 </h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
             </div>
@@ -173,29 +173,29 @@ include "html-elements/html_nav.php";?>
         <hr>
 
         <!-- Pagination -->
-        <div class="row text-center">
-            <div class="col-lg-12">
-                <ul class="pagination">
+        <div class='row text-center'>
+            <div class='col-lg-12'>
+                <ul class='pagination'>
                     <li>
-                        <a href="#">&laquo;</a>
+                        <a href='#'>&laquo;</a>
                     </li>
-                    <li class="active">
-                        <a href="#">1</a>
-                    </li>
-                    <li>
-                        <a href="#">2</a>
+                    <li class='active'>
+                        <a href='#'>1</a>
                     </li>
                     <li>
-                        <a href="#">3</a>
+                        <a href='#'>2</a>
                     </li>
                     <li>
-                        <a href="#">4</a>
+                        <a href='#'>3</a>
                     </li>
                     <li>
-                        <a href="#">5</a>
+                        <a href='#'>4</a>
                     </li>
                     <li>
-                        <a href="#">&raquo;</a>
+                        <a href='#'>5</a>
+                    </li>
+                    <li>
+                        <a href='#'>&raquo;</a>
                     </li>
                 </ul>
             </div>
@@ -206,8 +206,8 @@ include "html-elements/html_nav.php";?>
 
         <!-- Footer -->
         <footer>
-            <div class="row">
-                <div class="col-lg-12">
+            <div class='row'>
+                <div class='col-lg-12'>
                     <p>Copyright &copy; PluggTugg 2016</p>
                 </div>
             </div>
@@ -218,8 +218,8 @@ include "html-elements/html_nav.php";?>
     <!-- /.container -->
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src='js/jquery.js'></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-<?php include"html-elements/html_footer.php";?>
+    <script src='js/bootstrap.min.js'></script>
+<?php include'html-elements/html_footer.php';?>
