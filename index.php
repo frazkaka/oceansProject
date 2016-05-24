17cd1a6fa16c87c49a4b48ab11363790c9ce0195
frazkaka <mickefransson1@gmail.com>
2016-05-24 12:51:40 +0200

    dada
    dada

--- index.php
+++ index.php
@@ -3,10 +3,21 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 session_start();
 include 'html-elements/html_head.php';
-include 'html-elements/html_nav.php';?>
+
+include 'html-elements/html_nav.php';
+
+?>
 
     <div id='container'>
       <div id='leftContainer'>Top 10</div>
       <div id='rightContainer'>Flöde av bilder</div>
     </div>
+    <?php
+    if (isset($_SESSION['username'])){
+      echo "Welcome, '{$_SESSION['username']}'";
+    }else {
+      echo "Du är inte inloggad";
+    }
+
+    ?>
  <?php include'html-elements/html_footer.php';?>
