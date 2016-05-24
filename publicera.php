5614c30244a202cd7b9b7d4b6601286d7ba87cac
SexyTacos <andreas95englund@gmail.com>
2016-05-24 10:12:42 +0200

    Tillägg till senaste ändringen

--- publicera.php
+++ publicera.php
@@ -1,16 +1,17 @@
 <?php include 'html-elements/html_head.php';
 include 'html-elements/html_nav.php';?>
-
+<!-- Här ska det vara form men kategori-add funktionen fungerar inte då... -->
 <div class="container">
-
   <div class="row">
     <h2>Skapa ett recept!</h2>
     <div class="col-xs-12 well">
+        <form>
         <fieldset class="form-group">
           <label for="recipetitle">Titel</label>
           <input class="form-control" id="recipetitle" rows="1" maxlength="30">
           <small class="text-muted">Ge ditt recept ett kort och beskrivande namn. Börja med stor bokstav. (Max 30 tecken)</small>
         </fieldset>
+        </form>
         <fieldset>
           <div class="container">
             <div class="row">
@@ -19,17 +20,17 @@ include 'html-elements/html_nav.php';?>
                 <div class="controls">
                   <form role="form" autocomplete="off">
                     <div class="entry input-group col-xs-3">
-                      <select class="form-control" name="categories[]">
-                          <option value="Kött">Kött</option>
-                          <option value="Fågel">Fågel</option>
-                          <option value="Fisk/skaldjur">Fisk/skaldjur</option>
-                          <option value="Pasta">Pasta</option>
-                          <option value="Soppa/pajer">Soppa/pajer</option>
-                          <option value="Vegetariskt">Vegetariskt</option>
-                          <option value="Mackor/wraps">Mackor/wraps</option>
-                          <option value="Pannkakor/omelett">Pannkakor/omelett</option>
-                          <option value="Pannkakor/omelett">Mellanmål</option>
-                          <option value="Övrigt">Övrigt</option>
+                      <select class="form-control" name="fields[]">
+                          <option value="kött">Kött</option>
+                          <option value="fågel">Fågel</option>
+                          <option value="pisk/skaldjur">Fisk/skaldjur</option>
+                          <option value="pasta">Pasta</option>
+                          <option value="soppa/pajer">Soppa/pajer</option>
+                          <option value="vegetariskt">Vegetariskt</option>
+                          <option value="mackor/wraps">Mackor/wraps</option>
+                          <option value="pannkakor/omelett">Pannkakor/omelett</option>
+                          <option value="mellanmål">Mellanmål</option>
+                          <option value="övrigt">Övrigt</option>
                       </select>
                       <span class="input-group-btn">
                         <button class="btn btn-success btn-add" type="button">
@@ -37,7 +38,7 @@ include 'html-elements/html_nav.php';?>
                         </button>
                       </span>
                     </div>
-                  </form>
+
                   <small class="text-muted">Tryck på <span class="glyphicon glyphicon-plus gs"></span> för att lägga till en ny ingrediens.</small>
                 </div>
                 <br>
@@ -100,34 +101,13 @@ include 'html-elements/html_nav.php';?>
               <input type="radio">
             </label>
           </div>
-        </fieldset>
-        <fieldset>
-          <div class="container">
-            <div class="row">
-              <div class="control-group" id="fields">
-                <label class="control-label" for="field1">Ingredienser</label>
-                <div class="controls">
-                  <form role="form2" autocomplete="off">
-                    <div class="entry input-group col-xs-3">
-                      <input class="form-control" name="fields[]" type="text" placeholder="Ange mängd och ingrediens..." />
-                      <span class="input-group-btn">
-                        <button class="btn btn-success btn-add2" type="button">
-                          <span class="glyphicon glyphicon-plus"></span>
-                        </button>
-                      </span>
-                    </div>
-                  </form>
-                  <small class="text-muted">Tryck på <span class="glyphicon glyphicon-plus gs"></span> för att lägga till en ny ingrediens.</small>
-                </div>
-                <br>
-              </div>
-            </div>
-          </div>
-        </fieldset>
+        </fieldset
         <fieldset class="form-group">
-          <label for="recipe-editor-comments">Sammanfattning</label>
-          <textarea class="form-control" id="recipe-editor-comments" rows="2" maxlength="200"></textarea>
-          <small class="text-muted">Några personliga ord från dig om receptet. Det här texten som visas när folk bläddrar igenom recepeten på sidan. (Max 100 tecken)</small>
+          <div id="ingredients">
+          <label for="recipe-editor-comments">Ingredienser</label>
+          <textarea class="form-control" id="recipe-editor-comments" rows="10" maxlength="500"></textarea>
+          <small class="text-muted">Ange mängd och ingrediens. Exempel: 5 dl Vispgrädde.</small>
+        </div>
         </fieldset>
         <fieldset class="form-group">
           <label for="recipe-editor-directions">Beskrivning</label>
@@ -154,5 +134,5 @@ include 'html-elements/html_nav.php';?>
     </div>
   </div>
 </div>
-
+</form>
 <?php include'html-elements/html_footer.php';?>
