884615710fa6c5487880b62339c205b4511e41de
Antonia Miocic <a.miocic@hotmail.com>
2016-05-25 10:40:25 +0200

    Ändrat så att kommentarer är beroende på vilket recept man är inne på
    Ändrat så att kommentarer är beroende på vilket recept man är inne på

--- phpscripts/kommentarer.inc.php
+++ phpscripts/kommentarer.inc.php
@@ -10,8 +10,9 @@ include "database.inc.php";
 
 $username = $_SESSION['username'];
 $comment= $_POST['comment'];
+$idrecipe = '1';
 
-$sql= "INSERT INTO comment (comment, idUser) VALUES ('$comment', '$username')";
+$sql= "INSERT INTO comment (comment, idUser, idrecipe) VALUES ('$comment', '$username', '$idrecipe')";
 
 if ($conn->query($sql) === TRUE) {
     header("Location: ../activ_recipe.php");
