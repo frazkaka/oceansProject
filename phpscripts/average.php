3d480ad97dccd9cf758aa1f855dc3ce7234bf8c0
frazkaka <mickefransson1@gmail.com>
2016-05-27 14:39:02 +0200

    random, index.php mest
    hoppas det net går knas.

--- phpscripts/average.php
+++ phpscripts/average.php
@@ -14,11 +14,11 @@ while ($row = $result->fetch_array()){
     $count = count($kaboom);
     $sum = array_sum($kaboom);
     $avg= ($sum/$count);
-    $roundit =floor($avg);
-    
+    $roundit = floor($avg);
+
     $sql1= "UPDATE recipe SET average ='$roundit' WHERE idRecipe = '$active'";
     $conn->query($sql1);
-    
+
 
     if($roundit==0){
     $rating = "Detta recept har inte blivit betygsatt än";
