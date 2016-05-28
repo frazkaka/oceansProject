44690b30e3b9657c6806347db3a41b8334bd011f
SexyTacos <andreas95englund@gmail.com>
2016-05-28 15:06:35 +0200

    Små krix

--- user.php
+++ user.php
@@ -1,6 +1,23 @@
 <?php
 include 'html-elements/html_head.php';
 include 'html-elements/html_nav.php';
+include 'phpscripts/database.inc.php';
+
+//User info
+$idUser = 1;
+$sql ="SELECT * FROM user WHERE idUser='$idUser'";
+$result = $conn->query($sql) or die($conn->error);
+$row = $result->fetch_array(MYSQLI_ASSOC);
+
+//User recept
+$sql2 ="SELECT * FROM recipe WHERE idUser='$idUser'";
+$resultRec = $conn->query($sql2);
+$counter=0;
+while ($rowRec = $resultRec->fetch_array())
+{
+  $recepies[] = array('idRecipe' => $rowRec['idRecipe'], 'image' => $rowRec['image'], 'headline' => $rowRec['headline'], 'cost' => $rowRec['cost'], 'rating'=> $rowRec['rating'] );
+  $counter++;
+}
 ?>
 
 <div class="container">
@@ -10,7 +27,7 @@ include 'html-elements/html_nav.php';
         <div class="panel-body">
           <div class="row">
             <div class="col-xs-12 col-sm-4 text-center">
-              <img src="<?php echo $_SESSION['profileImage'];?>" alt="avatar" class="center-block img-circle img-thumbnail img-responsive">
+              <img src="<?php echo $row['userImage'];?>" alt="avatar" class="center-block img-circle img-thumbnail img-responsive">
               <ul class="list-inline ratings text-center" title="Ratings">
                 <li><a href="#"><span class="fa fa-star fa-lg"></span></a></li>
                 <li><a href="#"><span class="fa fa-star fa-lg"></span></a></li>
@@ -21,73 +38,68 @@ include 'html-elements/html_nav.php';
             </div>
             <!--/col-->
             <div class="col-xs-12 col-sm-8">
-              <h1><?php echo $_SESSION['username'];?></h1>
-              <p><strong>Om: </strong> Web Designer / UI Expert. </p>
-              <p><strong>Skola: </strong> <?php echo  $_SESSION['school'];?></p>
+              <h1><?php echo $row['username'];?></h1>
+              <p><strong>Om: </strong> <?php echo $row['about'];?> </p>
+              <p><strong>Skola: </strong> <?php echo  $row['school'];?></p>
+              <div class="row col-xs-5 col-md-3">
+                <a role="button" href='#följ' class="btn btn-success btn-block">Följ</a>
+              </div>
+              <div class="row col-xs-7 col-md-sm-4 col-md-3" style="margin-left: 5%;">
+                <a href="mailto:<?php echo $row['userEmail'];?>" class="btn btn-primary btn-block">Skicka meddelande</a>
+              </div>
+              <!--/col-->
+              <div class="clearfix"></div>
+              <div class="col-xs-12 col-sm-4">
+                <h2><strong> 20,7K </strong></h2>
+                <p>Följare</p>
+              </div>
+              <!--/col-->
+              <div class="col-xs-12 col-sm-4">
+                <h2><strong>245</strong></h2>
+                <p>Följer</p>
+              </div>
+              <!--/col-->
+              <div class="col-xs-12 col-sm-4">
+                <h2><strong><?php echo $counter; ?></strong></h2>
+                <p> Recpet</p>
+              </div>
+              <!--/col-->
             </div>
-            <!--/col-->
-            <div class="clearfix"></div>
-            <div class="col-xs-12 col-sm-4">
-              <h2><strong> 20,7K </strong></h2>
-              <p><small>Followers</small></p>
-              <button class="btn btn-success btn-block"><span class="fa fa-plus-circle"></span> Follow </button>
+            <!--/row-->
+            <div>
+              <h3 class='page-header row col-md-12' id='recepten'>Recept från <?php echo $row['username'];?></h3>
+              <div class='row'>
+                <?php
+
+                if(!empty($recepies)){
+                  foreach ($recepies as $recept){
+                    echo'<div class ="col-md-4 portfolio-item">';
+                    echo'<a href="#">';
+                    echo'<img class="img-responsive" src="'. $recept['image'].'" alt ="">';
+                    echo'</a>';
+                    echo'<a href="activ_recipe.php?id='.$recept['idRecipe'].'"><h3>'.$recept['headline'].'</h3></a>';
+                    echo'<p>Kostnad: '. $recept['cost'].'</p>';
+                    echo'<p>betyg: '. $recept['cost'].'</p>';
+
+                    echo '</div>';
+                  }
+                }
+                else{
+                  echo '<div class="col-md-12"><p>'.$row['username'].' har inte publicerat några recept.</p></div>';
+                }
+                ?>
+              </div>
             </div>
-            <!--/col-->
-            <div class="col-xs-12 col-sm-4">
-              <h2><strong>245</strong></h2>
-              <p><small>Following</small></p>
-              <button class="btn btn-info btn-block"><span class="fa fa-user"></span> View Profile </button>
-            </div>
-            <!--/col-->
-            <div class="col-xs-12 col-sm-4">
-              <h2><strong>43</strong></h2>
-              <p><small>Snippets</small></p>
-              <a role="button" href='edit-profile.php' class="btn btn-primary btn-block">Redigera profil</a>
-            </div>
-            <!--/col-->
           </div>
-          <!--/row-->
-          <h3 id='recepten'>Recept från <?php echo $_SESSION['username'];?></h3>
-          <div class='row'>
-
-          <?php
-          include('phpscripts/database.inc.php');
-
-          $idUser = $_SESSION['idUser'];
-          $sql ="SELECT * FROM recipe WHERE idUser='$idUser'";
-          $result = $conn->query($sql);
-
-          while ($row = $result->fetch_array())
-          {
-            $recepies[] = array('idRecipe' => $row['idRecipe'], 'image' => $row['image'], 'headline' => $row['headline'], 'cost' => $row['cost'], 'rating'=> $row['rating'] );
-          }
-
-          foreach ($recepies as $recept){
-              echo'<div class ="col-md-4 portfolio-item">';
-              echo'<a href="#">';
-              echo'<img class="img-responsive" src="'. $recept['image'].'" alt ="">';
-              echo'</a>';
-              echo'<a href="activ_recipe.php?id='.$recept['idRecipe'].'"><h3>'.$recept['headline'].'</h3></a>';
-              echo'<p>Kostnad: '. $recept['cost'].'</p>';
-              echo'<p>betyg: '. $recept['cost'].'</p>';
-
-              echo '</div>';
-
-          }
-          //echo '</div>';
-
-          ?>
+          <!--/panel-body-->
         </div>
-        </div>
-        <!--/panel-body-->
+        <!--/panel-->
       </div>
-      <!--/panel-->
+      <!--/col-->
     </div>
-    <!--/col-->
+    <!--/row-->
   </div>
-  <!--/row-->
-</div>
-<!--/container-->
+  <!--/container-->
 
-    <!-- ----------------------------------------------- -->
+  <!-- ----------------------------------------------- -->
   <?php include'html-elements/html_footer.php';?>
